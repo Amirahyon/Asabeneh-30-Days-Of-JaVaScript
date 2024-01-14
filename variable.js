fd901027812d44e6450d2a628aab35a70f5d933d
Amirahyon <amirahyon@gmail.com>
2024-01-14 18:52:37 +0100

    Day 1

---
+++ variable.js
@@ -0,0 +1,25 @@
+let firstName ="Amirah";
+console.log(firstName);
+let lastName ="Abdullahi";
+console.log(lastName);
+let country ="Nigeria";
+console.log(country);
+let city ="Ilorin";
+console.log(city);
+let age = 21;
+console.log(age);
+let isMarried = false;
+let job ="Student";
+console.log(job);
+
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof isMarried);
+
+let town =undefined;
+console.log(town);
+console.log(typeof town);
+
+let home =null;
+console.log(home);
+console.log(typeof home);

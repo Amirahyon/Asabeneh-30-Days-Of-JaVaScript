80ce95b8f76719fb662f112931adebffa064b0f5
Amirahyon <amirahyon@gmail.com>
2024-02-08 17:31:18 +0100

    Day 3

--- Day3/main.js
+++ Day3/main.js
@@ -50,32 +50,32 @@ console.log(date.getMinutes());
 console.log(date.getTime());
 
 // Level 2
-// const a = prompt("Enter base of a triangle");
-// const b = prompt("Enter height of a triangle");
-// const area = 0.5 * a * b;
-// console.log(area);
+const a = prompt("Enter base of a triangle");
+const b = prompt("Enter height of a triangle");
+const area = 0.5 * a * b;
+console.log(area);
 
-// const x = prompt("Enter side 'a' of the triangle");
-// const y = prompt("Enter side 'b' of the triangle");
-// const z = prompt("Enter side 'c' of the triangle");
-// const perimeter = +x + +y + +z;
-// console.log(perimeter);
+const e = prompt("Enter side 'e' of the triangle");
+const f = prompt("Enter side 'f' of the triangle");
+const g = prompt("Enter side 'g' of the triangle");
+const perimeter = +e + +f + +g;
+console.log(perimeter);
 
-// const lenght = prompt("Enter length");
-// const width = prompt("Enter width");
-// const areaOfTriangle = length * width;
-// console.log(areaOfTriangle);
+const lenght = prompt("Enter length");
+const width = prompt("Enter width");
+const areaOfTriangle = length * width;
+console.log(areaOfTriangle);
 
-// const perimeterOfATriangle = 2 * (Number(length) + Number(width));
-// console.log(perimeterOfATriangle);
+const perimeterOfATriangle = 2 * (Number(length) + Number(width));
+console.log(perimeterOfATriangle);
 
-// const radius = prompt("Enter radius");
-// const PI = Math.PI
-// areaOfCircle = PI * radius * radius;
-// console.log(areaOfCircle);
+const radius = prompt("Enter radius");
+const PI = Math.PI
+areaOfCircle = PI * radius * radius;
+console.log(areaOfCircle);
 
-// circumference = 2 * PI * radius;
-// console.log(circumference);
+circumference = 2 * PI * radius;
+console.log(circumference);
 
 let y2 = 2;
 let y1 = 2;
@@ -88,10 +88,10 @@ let x = 1;
 let y = (x ** 2) + (6 * x) + 9;
 console.log(y);
 
-// const hours = prompt("Enter hours");
-// const ratePerHour = prompt("Enter rate");
-// const pay = ratePerHour * hours;
-// console.log(pay);
+const hours = prompt("Enter hours");
+const ratePerHour = prompt("Enter rate");
+const pay = ratePerHour * hours;
+console.log(pay);
 
 let name = "Amirah";
 name.length > 7
@@ -107,16 +107,16 @@ name.length > 7
  let yourAge = 50;
  console.log('I am', `${yourAge - myAge}`, 'younger than you.');
 
-//  let birthYear = prompt("Enter your birth year");
-//  let remainingYears = 18 - birthYear;
-//  birthYear >= 18
-//  ? console.log("You are allowed to drive"):
-//   console.log("You have", `${remainingYears}`, "left to drive");
+ let birthYear = prompt("Enter your birth year");
+ let remainingYears = 18 - birthYear;
+ birthYear >= 18
+ ? console.log("You are allowed to drive"):
+  console.log("You have", `${remainingYears}`, "left to drive");
 
-// let numberOfYears = prompt("Enter number of years youve lived");
+let numberOfYears = prompt("Enter number of years youve lived");
 
 
-
+// Level 3
 console.log(date);
 year = date.getFullYear();
 month = date.getMonth() + 1;

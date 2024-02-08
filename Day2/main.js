1f9bc5d6376ba7f0adc3ac6d3b81315d0f1f36cf
Amirahyon <amirahyon@gmail.com>
2024-02-08 15:08:51 +0100

    Day 2

--- Day2/main.js
+++ Day2/main.js
@@ -60,5 +60,37 @@ console.log(python.includes("on"));
 random = Math.random()
 randomNum = random * 100;
 console.log(randomNum);
-randoMnum = (random + 50);
+randoMnum = Math.random() * 50 + 50;
 console.log(randoMnum);
+
+randomNumBtwn = random * 255;
+console.log(randomNumBtwn);
+
+console.log("1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125 ");
+
+console.log(sentence.indexOf("because"));
+console.log(sentence.lastIndexOf("because"));
+console.log(sentence.substr(31, 23));
+
+
+// Level 3
+let l = 'Love is the best thing in this world. Some found their love and some are still looking for their love.';
+love = /love/gi;
+console.log(l.match(love));
+
+because = /because/gi;
+console.log(sentence.match(because));
+
+const sentenc = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
+let inequalities = /[%@&$#;]/gi;
+console.log(sentenc.replace(inequalities, ""));
+
+const annualIncome = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.';
+let income = /\d+/gi;
+console.log(annualIncome.match(income));
+let a = annualIncome.match(income);
+let a1 = Number(a[0] * 12);
+let a2 = Number(a[1]);
+let a3 = Number(a[2] * 12);
+console.log(a1 + a2 + a3);
+console.log(+(a[0] * 12) + +(a[1]) + +(a[2] * 12));
